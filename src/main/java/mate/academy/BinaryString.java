138f80e23f98c779debb2bf1cf794d9e2f3e16ef
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-03-17 10:35:54 +0200

    add binary String

--- src/main/java/mate/academy/BinaryString.java
+++ src/main/java/mate/academy/BinaryString.java
@@ -3,9 +3,17 @@ package mate.academy;
 public class BinaryString {
 
     public static void main(String[] args) {
-        toBinaryString(2);
+        System.out.println(toBinaryString(23));
     }
     public static String toBinaryString(int value) {
-        return "test";
+        int x = value;
+        int y;
+        StringBuilder builder = new StringBuilder();
+        while (x != 0) {
+            y = x % 2;
+            x = x / 2;
+            builder.append(y);
+        }
+        return builder.reverse().toString();
     }
 }
