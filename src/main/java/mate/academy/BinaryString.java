34c3190e19086f0a14ac79d0a4a6b6690282f0b0
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-03-17 10:11:49 +0200

    first commit

---
+++ src/main/java/mate/academy/BinaryString.java
@@ -0,0 +1,11 @@
+package mate.academy;
+
+public class BinaryString {
+
+    public static void main(String[] args) {
+        toBinaryString(2);
+    }
+    public static String toBinaryString(int value) {
+        return "test";
+    }
+}
